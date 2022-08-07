0981c11caaafea18f9d165853483dd2b28a22fb8
tonygiang <tony_s_giang@yahoo.com>
2022-08-07 13:32:01 +0700

    Project push.

---
+++ IEnumerator.LoopNext.cs
@@ -0,0 +1,49 @@
+﻿// A part of the C# Language Syntactic Sugar suite.
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CLSS
+{
+  public static partial class IEnumeratorLoopNext
+  {
+    /// <summary>
+    /// If the source enumerator is currently at the end of the collection, sets
+    /// the enumerator to its initial position, which is before the first
+    /// element in the collection. Then advances the enumerator to the next
+    /// element of the collection regardless of whether it has been reset.
+    /// </summary>
+    /// <typeparam name="T">The type of objects to enumerate.</typeparam>
+    /// <param name="enumerator">The source enumerator.</param>
+    /// <returns>Always true.</returns>
+    public static bool LoopNext<T>(this IEnumerator<T> enumerator)
+    {
+      if (!enumerator.MoveNext()) { enumerator.Reset(); enumerator.MoveNext(); }
+      return true;
+    }
+
+    /// <summary>
+    /// Advances the source enumerator to the next element of the collection in
+    /// a loop and returns the object at that position.
+    /// </summary>
+    /// <typeparam name="T"><inheritdoc cref="LoopNext{T}(IEnumerator{T})"
+    /// path="/typeparam[@name='T']"/></typeparam>
+    /// <param name="enumerator">
+    /// <inheritdoc cref="LoopNext{T}(IEnumerator{T})"
+    /// path="/param[@name='enumerator']"/></param>
+    /// <returns>The next element in the enumeration.</returns>
+    public static T LoopNextElement<T>(this IEnumerator<T> enumerator)
+    { enumerator.LoopNext(); return enumerator.Current; }
+
+    /// <inheritdoc cref="LoopNext{T}(IEnumerator{T})"/>
+    public static bool LoopNext(this IEnumerator enumerator)
+    {
+      if (!enumerator.MoveNext()) { enumerator.Reset(); enumerator.MoveNext(); }
+      return true;
+    }
+
+    /// <inheritdoc cref="LoopNextElement{T}(IEnumerator{T})"/>
+    public static object Pop(this IEnumerator enumerator)
+    { enumerator.LoopNext(); return enumerator.Current; }
+  }
+}
---
+++ README.md
@@ -0,0 +1,47 @@
+﻿# CLSS.ExtensionMethods.IEnumerator.LoopNext
+
+### Problem
+
+Looping a collection indefinitely is an operation that maps well to many use cases (such as AI behavior phases, sequentially looping slideshow, infinite scrolling background,  etc...), but since there is no native way to perform this type of loop, you have to write some non-obvious code to do so.
+
+```
+var numbers = new int[] { 0, 1, 2, 3 };
+int li = 0;
+foreach (int i = 0; i < 10; ++i)
+{
+  var number = numbers[li];
+  // do something wih number
+  ++li;
+  if (li == numbers.Length) li = 0;
+}
+```
+
+### Solution
+
+`LoopNext` is an extension method that infinitely advances an enumerator in a loop. The above code segment can be rewritten as follows:
+
+```
+using CLSS;
+
+var numbers = new int[] { 0, 1, 2, 3 };
+var numbersEnumerator = numbers.GetEnumerator();
+foreach (int i = 0; i < 10; ++i)
+{
+  numbersEnumerator.LoopNext();
+  DoSomethingWith(numbersEnumerator.Current);
+}
+```
+
+Because `LoopNext` always returns true, the usual step of checking its result before accessing the `Current` property can be skipped altogether.
+
+Since it then becomes natural to call `LoopNext` and accessing `Current` back-to-back, this package also comes with a `LoopNextElement` extension method that combines these 2 actions, moving to the next element in a loop directly:
+
+```
+using CLSS;
+
+DoSomethingWith(numbersEnumerator.LoopNextElement());
+```
+
+You can visualize `LoopNextElement` as "popping off" one object from the front end of an infinite queue (that lazily evaluates).
+
+##### This package is a part of the [C# Language Syntactic Sugar suite](https://github.com/tonygiang/CLSS).
